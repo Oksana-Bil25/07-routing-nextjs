a3aca7277e6bfa214174dee09ade08c3e30f1452
Oksana-Bil25 <vikos24g@gmail.com>
2025-12-23 21:20:42 +0200

    fix missing api exports and types

--- app/notes/layout.tsx
+++ app/notes/layout.tsx
@@ -1,21 +1,7 @@
-import styles from "./LayoutNotes.module.css";
 export default function NotesLayout({
   children,
-  modal,
-  sidebar,
 }: {
   children: React.ReactNode;
-  modal: React.ReactNode;
-  sidebar: React.ReactNode;
 }) {
-  return (
-    <div className={styles.container}>
-      <aside className={styles.sidebar}>{sidebar}</aside>
-
-      <main className={styles.notesWrapper}>
-        {children}
-        {modal}
-      </main>
-    </div>
-  );
+  return <div>{children}</div>;
 }
